8df2f4020eb3279300fe346554aca4a417019005
sinh <nsinh2281@gmail.com>
2025-03-26 19:32:15 +1100

    update profilepic to cloudanery and change it in dynamo db + delete user

--- backend/src/routes/users.route.ts
+++ backend/src/routes/users.route.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllUser ,getSingleUser, getAllUserDocuments,getSingleUserDocument} from '../controller/users.controller'
+import { getAllUser ,getSingleUser, getAllUserDocuments,getSingleUserDocument,updateProfilePic,deleteUser} from '../controller/users.controller'
 import { userAuth } from '../middleware/userAuth'
 
 export const router = express.Router()
@@ -8,3 +8,5 @@ router.get('/getAllUser',userAuth, getAllUser)
 router.get('/getUserById/:id',userAuth, getSingleUser)
 router.get('/AllUserDocuments',userAuth, getAllUserDocuments)
 router.get('/SingleUserDocument/:documentID',getSingleUserDocument)
+router.put('/update-profile', userAuth, updateProfilePic)
+router.delete('/delete-user', userAuth, deleteUser); // need to be fixed, comment for me only since no fking one but me work on backend 
