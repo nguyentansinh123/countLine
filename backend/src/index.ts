c2b548c8cff137992fa29d70ead8904e63f9189d
sinh <nsinh2281@gmail.com>
2025-04-03 01:04:58 +1100

    bring code to normal

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,35 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import { PutCommand } from "@aws-sdk/lib-dynamodb";
-import { docClient } from "./lib/dynamoClient";
-import { router as AuthRoute } from "./routes/auth.route";
-import { router as UserRoute } from "./routes/users.route";
-import { router as DocumentRoute } from "./routes/document.route";
-import { router as TeamRoute } from "./routes/team.route";
-import { router as ProjectRoute } from "./routes/project.route";
+import { docClient } from './lib/dynamoClient';
+import {router as AuthRoute} from './routes/auth.route'
+import {router as UserRoute} from './routes/users.route'
+import {router as DocumentRoute} from './routes/document.route'
+import {router as TeamRoute} from './routes/team.route'
+import {router as ProjectRoute} from './routes/project.route'
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 5001;
 
-const allowedOrigins = ["http://localhost:5173"];
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+const allowedOrigins = ['http://localhost:5173'];
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth", AuthRoute);
-app.use("/api/users", UserRoute);
-app.use("/api/document", DocumentRoute);
-app.use("/api/team", TeamRoute);
-app.use("/api/project", ProjectRoute);
+app.use('/api/auth', AuthRoute)
+app.use('/api/users', UserRoute)
+app.use('/api/document', DocumentRoute)
+app.use('/api/team', TeamRoute)
+app.use('/api/project', ProjectRoute)
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
